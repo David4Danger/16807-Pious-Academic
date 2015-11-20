e810d39a5888b1d99be3244f2d13f2915c887347
David Skudra <dskudra@gmail.com>
2015-11-20 09:27:22 -0500

    Updated metadata to include BTB static data

--- getmeta.py
+++ getmeta.py
@@ -7,81 +7,81 @@ def main():
     
     campaign_missions = api.get_campaign_missions()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\campaignmissions.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\campaignmissions.json', 'w') as fp:
         json.dump(campaign_missions, fp)    
             
     commendations = api.get_commendations()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\commendations.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\commendations.json', 'w') as fp:
         json.dump( commendations, fp)
                             
     csr_designations = api.get_csr_designations()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\CSRdesignations.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\CSRdesignations.json', 'w') as fp:
         json.dump(csr_designations, fp)    
                             
     enemies = api.get_enemies()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\enemies.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\enemies.json', 'w') as fp:
         json.dump(enemies, fp)    
                             
     flexible_stats = api.get_flexible_stats()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\flexiblestats.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\flexiblestats.json', 'w') as fp:
         json.dump(flexible_stats, fp)        
         
     game_base_variants = api.get_game_base_variants()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\gamebasevariants.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\gamebasevariants.json', 'w') as fp:
         json.dump(game_base_variants, fp)        
                             
     impulses = api.get_impulses()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\impulses.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\impulses.json', 'w') as fp:
         json.dump(impulses, fp)  
                             
     maps = api.get_maps()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\maps.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\maps.json', 'w') as fp:
         json.dump(maps, fp)  
                             
     medals = api.get_medals()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\medals.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\medals.json', 'w') as fp:
         json.dump(medals, fp)       
                             
     playlists = api.get_playlists()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\playlists.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\playlists.json', 'w') as fp:
         json.dump(playlists, fp)  
                             
     skulls = api.get_skulls()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\skulls.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\skulls.json', 'w') as fp:
         json.dump(skulls, fp)       
                             
     spartan_ranks = api.get_spartan_ranks()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\spartanranks.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\spartanranks.json', 'w') as fp:
         json.dump(spartan_ranks, fp)      
                             
     team_colors = api.get_team_colors()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\teamcolors.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\teamcolors.json', 'w') as fp:
         json.dump(team_colors, fp)     
                             
     vehicles = api.get_vehicles()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\vehicles.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\vehicles.json', 'w') as fp:
         json.dump(vehicles, fp)    
                             
     weapons = api.get_weapons()
     time.sleep(1)
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\weapons.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\weapons.json', 'w') as fp:
         json.dump(weapons, fp)    
     
     #now use the above static data to execute the variant meta requests
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\maps.json') as data_file:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\maps.json') as data_file:
         mapfile = json.load(data_file)
     mapvariants = {}
     for item in mapfile:
@@ -90,15 +90,14 @@ def main():
                 mapid = item['id']
                 variantinfo = api.get_maps_variants_by_id(mapid)
                 mapvariants[mapid] = variantinfo
-                print(len(mapvariants))
             except:
                 time.sleep(9)
                 continue
             break
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\mapvariants.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\mapvariants.json', 'w') as fp:
         json.dump(mapvariants, fp)
         
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\gamebasevariants.json') as data_file:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\gamebasevariants.json') as data_file:
         gamefile = json.load(data_file)
     gamevariants = {}
     for item in gamefile:
@@ -107,12 +106,11 @@ def main():
                 gameid = item['id']
                 variantinfo = api.get_game_variants_by_id(gameid)
                 gamevariants[gameid] = variantinfo
-                print(len(gamevariants))
             except:
                 time.sleep(9)
                 continue
             break
-    with open(r'E:\Python Programs\BTB.net Work\Metadata\gamevariants.json', 'w') as fp:
+    with open(r'C:\Users\David\Documents\Github\16807-Pious-Academic\Metadata\gamevariants.json', 'w') as fp:
         json.dump(gamevariants, fp)    
     
     #Still can't do these meta request because there is no actual endpoint that returns all the ids :*[                    
